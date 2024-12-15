d3ac6a7ac1908382cc13b65a954b11a8280fead4
zagrebaliakskill <orshan1003@gmail.com>
2024-12-15 17:04:54 +0300

    day-14 finish

--- day-14/main.js
+++ day-14/main.js
@@ -1,1 +1,9 @@
 import "./styles.css";
+
+const hRangeChange = (e) => {
+    const value = e.target.value
+
+    document.body.style.setProperty("--base-hue", value)
+}
+
+document.querySelector("#range-input").addEventListener("change", hRangeChange)
